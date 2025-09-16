47a873563c5c1dad81ebc1ff6a6083bb0db7e42b
Carlos Marques <carrlosmr@gmail.com>
2025-09-16 20:31:48 -0300

    authentication module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { EnvironmentModule } from './config/environment/environment.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    EnvironmentModule,
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
